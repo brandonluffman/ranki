b2cd82099b70cfa79e18b35b6c7fee15f2bf282b
Brandon Luffman <brandonluff10@gmail.com>
2023-12-05 16:06:03 -0500

    removed google credentials

--- pages/api/auth/url.js
+++ pages/api/auth/url.js
@@ -1,17 +1,17 @@
-// pages/api/auth/url.js
-import { google } from 'googleapis';
+// // pages/api/auth/url.js
+// import { google } from 'googleapis';
 
-export default function handler(req, res) {
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback`
-  );
+// export default function handler(req, res) {
+//   const oauth2Client = new google.auth.OAuth2(
+//     process.env.GOOGLE_CLIENT_ID,
+//     process.env.GOOGLE_CLIENT_SECRET,
+//     `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback`
+//   );
 
-  const url = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: ['https://www.googleapis.com/auth/webmasters.readonly']
-  });
+//   const url = oauth2Client.generateAuthUrl({
+//     access_type: 'offline',
+//     scope: ['https://www.googleapis.com/auth/webmasters.readonly']
+//   });
 
-  res.redirect(url);
-}
+//   res.redirect(url);
+// }
--- pages/api/searchConsole.js
+++ pages/api/searchConsole.js
@@ -1,29 +1,29 @@
-import { google } from 'googleapis';
+// import { google } from 'googleapis';
 
-export default async function handler(req, res) {
-    console.log('Made It')
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback`
-  );
+// export default async function handler(req, res) {
+//     console.log('Made It')
+//   const oauth2Client = new google.auth.OAuth2(
+//     process.env.GOOGLE_CLIENT_ID,
+//     process.env.GOOGLE_CLIENT_SECRET,
+//     `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback`
+//   );
 
-  const userTokens = req.session.tokens; // Assuming you're using sessions
+//   const userTokens = req.session.tokens; // Assuming you're using sessions
 
-  if (userTokens) {
-    oauth2Client.setCredentials(userTokens);
-  } else {
-    return res.status(401).json({ error: 'Authentication tokens not found' });
-  }
-  const searchConsole = google.webmasters({
-    version: 'v3',
-    auth: oauth2Client
-  });
+//   if (userTokens) {
+//     oauth2Client.setCredentials(userTokens);
+//   } else {
+//     return res.status(401).json({ error: 'Authentication tokens not found' });
+//   }
+//   const searchConsole = google.webmasters({
+//     version: 'v3',
+//     auth: oauth2Client
+//   });
 
-  try {
-    const response = await searchConsole.sites.list();
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-}
+//   try {
+//     const response = await searchConsole.sites.list();
+//     res.status(200).json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// }
