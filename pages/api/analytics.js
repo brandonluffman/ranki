c949fc8a86b856368b8ede02f33c0d3c229675c6
Brandon Luffman <brandonluff10@gmail.com>
2023-12-05 16:07:21 -0500

    autosacve

--- pages/api/analytics.js
+++ pages/api/analytics.js
@@ -1,28 +1,28 @@
-// pages/api/analytics.js
-const { BetaAnalyticsDataClient } = require('@google-analytics/data');
+// // pages/api/analytics.js
+// const { BetaAnalyticsDataClient } = require('@google-analytics/data');
 
-export default async function handler(req, res) {
-    // Path to your credentials JSON file
-    const credentials = require('../../data/credentials.json');
+// export default async function handler(req, res) {
+//     // Path to your credentials JSON file
+//     const credentials = require('../../data/credentials.json');
 
-    const analyticsDataClient = new BetaAnalyticsDataClient({
-        credentials: credentials
-    });
+//     const analyticsDataClient = new BetaAnalyticsDataClient({
+//         credentials: credentials
+//     });
 
-    // Replace with your Google Analytics 4 property ID
-    const propertyId = process.env.NEXT_PUBLIC_GA4_PROPERTY_ID;
-    try {
-        const [response] = await analyticsDataClient.runReport({
-            property: `properties/${propertyId}`,
-            dateRanges: [{ startDate: '7daysAgo', endDate: 'today' }],
-            metrics: [{ name: 'activeUsers' }],
-            // Add additional parameters as needed
-        });
+//     // Replace with your Google Analytics 4 property ID
+//     const propertyId = process.env.NEXT_PUBLIC_GA4_PROPERTY_ID;
+//     try {
+//         const [response] = await analyticsDataClient.runReport({
+//             property: `properties/${propertyId}`,
+//             dateRanges: [{ startDate: '7daysAgo', endDate: 'today' }],
+//             metrics: [{ name: 'activeUsers' }],
+//             // Add additional parameters as needed
+//         });
 
-        // Process and respond with the data
-        res.status(200).json(response);
-    } catch (error) {
-        console.error('Error fetching data from Google Analytics:', error);
-        res.status(500).json({ error: error.message });
-    }
-}
+//         // Process and respond with the data
+//         res.status(200).json(response);
+//     } catch (error) {
+//         console.error('Error fetching data from Google Analytics:', error);
+//         res.status(500).json({ error: error.message });
+//     }
+// }
--- pages/api/auth/callback.js
+++ pages/api/auth/callback.js
@@ -1,22 +1,22 @@
-// pages/api/auth/callback.js
-import { google } from 'googleapis';
+// // pages/api/auth/callback.js
+// import { google } from 'googleapis';
 
-export default async function handler(req, res) {
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback`
-  );
+// export default async function handler(req, res) {
+//   const oauth2Client = new google.auth.OAuth2(
+//     process.env.GOOGLE_CLIENT_ID,
+//     process.env.GOOGLE_CLIENT_SECRET,
+//     `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback`
+//   );
 
-  try {
-    const { tokens } = await oauth2Client.getToken(req.query.code);
-    oauth2Client.setCredentials(tokens);
+//   try {
+//     const { tokens } = await oauth2Client.getToken(req.query.code);
+//     oauth2Client.setCredentials(tokens);
 
-    // Save tokens in user session or database
-    req.session.tokens = tokens; // Make sure you have session handling set up
+//     // Save tokens in user session or database
+//     req.session.tokens = tokens; // Make sure you have session handling set up
 
-    res.redirect('/test'); // Replace with your dashboard route
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-}
+//     res.redirect('/test'); // Replace with your dashboard route
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// }
