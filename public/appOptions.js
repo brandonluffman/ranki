5719133531aaa4401f3770dce09f4c1d3d20ef60
Brandon Luffman <brandonluff10@gmail.com>
2023-12-06 22:19:22 -0500

    new

---
+++ public/appOptions.js
@@ -0,0 +1,40 @@
+const appOptions = [
+    {
+      name: "Vercel",
+      imgSrc: "/vercel.png" // Replace with the actual path to your image
+    },
+    {
+      name: "Proton",
+      imgSrc: "/proton.png" // Replace with the actual path to your image
+    },
+    {
+      name: "Google",
+      imgSrc: "/google.png" // Replace with the actual path to your image
+    },
+    {
+      name: "Namecheap",
+      imgSrc: "/namecheap.png" // Replace with the actual path to your image
+    },
+    {
+      name: "Supabase",
+      imgSrc: "/supabase.png" // Replace with the actual path to your image
+    },
+    {
+      name: "Stripe",
+      imgSrc: "/stripe.png" // Replace with the actual path to your image
+    },
+    {
+      name: "Github",
+      imgSrc: "/github.png" // Replace with the actual path to your image
+    },
+    {
+      name: "Google Analytics",
+      imgSrc: "/google-analytics.png" // Replace with the actual path to your image
+    },
+    {
+      name: "Heroku",
+      imgSrc: "/heroku.png" // Replace with the actual path to your image
+    },
+  ];
+
+  export default appOptions;
