74703854bbac227df52ee72b3077d31e25794607
Brandon Luffman <brandonluff10@gmail.com>
2024-01-24 15:50:50 -0500

    new

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,7 +1,34 @@
+import Head from 'next/head'
 import React from 'react'
 
 const privacy = () => {
   return (
+    <>
+           <Head>
+          <title>RankiAI | Privacy Policy</title>
+          <meta name="description" content="Empower your business with RankiAI's AI Blog Generator. Designed for businesses seeking growth, our platform delivers top-tier, SEO-optimized content to boost your website's traffic and visibility. Harness the power of AI to elevate your brand and attract more customers. Start transforming your content strategy today!" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" href="/favicon.png" />
+          <link rel="apple-touch-icon" href="/favicon.png" /> 
+          <link rel="canonical" href="https://ranki.ai/dashboard"/>
+          <meta property="og:type" content="article" />
+           <meta property="og:title" content="RankiAI | Privacy Policy" />
+           <meta property="og:description" content="Empower your business with RankiAI's AI Blog Generator. Designed for businesses seeking growth, our platform delivers top-tier, SEO-optimized content to boost your website's traffic and visibility. Harness the power of AI to elevate your brand and attract more customers. Start transforming your content strategy today!" />
+           <meta property="og:image" content="/favicon.png" />
+           <meta property="og:url" content="https://ranki.ai/privacy" />
+           <meta property="og:site_name" content="RankiAI" />
+           {/* <script
+           type="application/ld+json"
+           dangerouslySetInnerHTML={{ __html: JSON.stringify({
+               "@context": "https://schema.org",
+               "@type": "Organization",
+               "url": "https://www.rankiai.com",
+               "logo": "https://www.ranki.ai/public/favicon.png"
+             })}}
+         /> */}
+        </Head>
     <div>
         <h2>Privacy Policy</h2>
 <p>Last updated: January 15, 2024</p>
@@ -182,6 +209,7 @@ const privacy = () => {
 </li>
 </ul>
     </div>
+    </>
   )
 }
 
--- pages/terms.js
+++ pages/terms.js
@@ -1,7 +1,35 @@
+import Head from 'next/head'
 import React from 'react'
 
 const terms = () => {
   return (
+    <>
+           <Head>
+          <title>RankiAI | Terms of Use</title>
+          <meta name="description" content="Empower your business with RankiAI's AI Blog Generator. Designed for businesses seeking growth, our platform delivers top-tier, SEO-optimized content to boost your website's traffic and visibility. Harness the power of AI to elevate your brand and attract more customers. Start transforming your content strategy today!" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" href="/favicon.png" />
+          <link rel="apple-touch-icon" href="/favicon.png" /> 
+          <link rel="canonical" href="https://ranki.ai/terms"/>
+          <meta property="og:type" content="article" />
+           <meta property="og:title" content="RankiAI | Terms of Use" />
+           <meta property="og:description" content="Empower your business with RankiAI's AI Blog Generator. Designed for businesses seeking growth, our platform delivers top-tier, SEO-optimized content to boost your website's traffic and visibility. Harness the power of AI to elevate your brand and attract more customers. Start transforming your content strategy today!" />
+           <meta property="og:image" content="/favicon.png" />
+           <meta property="og:url" content="https://ranki.ai/terms" />
+           <meta property="og:site_name" content="RankiAI" />
+           {/* <script
+           type="application/ld+json"
+           dangerouslySetInnerHTML={{ __html: JSON.stringify({
+               "@context": "https://schema.org",
+               "@type": "Organization",
+               "url": "https://www.rankiai.com",
+               "logo": "https://www.ranki.ai/public/favicon.png"
+             })}}
+         /> */}
+        </Head>
+  
     <div>
         <h2><strong>Terms and Conditions</strong></h2>
 
@@ -121,6 +149,7 @@ const terms = () => {
     
     <p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
     </div>
+    </>
   )
 }
 
